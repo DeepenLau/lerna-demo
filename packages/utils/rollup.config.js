f8aebffb0cdd00c0da9a71c765078dd18bc48215
Deepen Lau <lpd09009@gmail.com>
2020-02-26 11:46:22 +0800

    test

--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -1,21 +1,23 @@
 import babel from 'rollup-plugin-babel';
+import { terser } from "rollup-plugin-terser";
 
 module.exports = {
   input: 'src/index.js',
   output: [
     {
       name: 'devopsUtils.js', // 生成UMD模块的名字
-      file: 'lib/utils.umd.js',
+      file: 'lib/utils.umd.min.js',
       format: 'umd'
     },
     {
-      file: 'lib/utils.esm.js',
+      file: 'lib/utils.esm.min.js',
       format: 'esm'
     },
   ],
   plugins: [
     babel({
       exclude: 'node_modules/**' // 只编译我们的源代码
-    })
+    }),
+    terser(),
   ]
 };
